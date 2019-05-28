343138b735abca74003811114db8730d7d932ab7
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2019-05-28 22:18:29 +0200

    Add pipe and log, introduce basic test for pipe.

--- src/__tests__/fun.test.js
+++ src/__tests__/fun.test.js
@@ -1,65 +1,100 @@
-const { map, flatMap, withDefault, mapError } = require('../fun');
+const { map, flatMap, withDefault, mapError, pipe, log } = require('../fun');
 
 describe('map', () => {
-	it('should transform the value', () => {
-		expect(map((x) => x + ', World!')([ null, 'Hello' ])).toMatchObject([ null, 'Hello, World!' ]);
-	});
+  it('should transform the value', () => {
+    expect(map(x => x + ', World!')([null, 'Hello'])).toMatchObject([
+      null,
+      'Hello, World!'
+    ]);
+  });
 
-	it('should return the error if the input contained the error', () => {
-		expect(map((x) => x + ', World!')([ new Error('Failed to provide input'), null ])).toMatchObject([
-			new Error('Failed to provide input'),
-			null
-		]);
-	});
+  it('should return the error if the input contained the error', () => {
+    expect(
+      map(x => x + ', World!')([new Error('Failed to provide input'), null])
+    ).toMatchObject([new Error('Failed to provide input'), null]);
+  });
 
-	it('should return the error if the mapping fn throws', () => {
-		expect(
-			map((x) => {
-				throw new Error('Booho!');
-			})([ null, true ])
-		).toMatchObject([ new Error('Booho!'), null ]);
-	});
+  it('should return the error if the mapping fn throws', () => {
+    expect(
+      map(x => {
+        throw new Error('Booho!');
+      })([null, true])
+    ).toMatchObject([new Error('Booho!'), null]);
+  });
 });
 
 describe('flatMap', () => {
-	it('should transform the value', () => {
-		expect(flatMap((x) => [ null, x + ', World!' ])([ null, 'Hello' ])).toMatchObject([ null, 'Hello, World!' ]);
-	});
+  it('should transform the value', () => {
+    expect(flatMap(x => [null, x + ', World!'])([null, 'Hello'])).toMatchObject(
+      [null, 'Hello, World!']
+    );
+  });
 
-	it('should return the error if the input contained the error', () => {
-		expect(flatMap((x) => [ null, 'This worked!' ])([ new Error('Sorry! Got an error.'), null ])).toMatchObject([
-			new Error('Sorry! Got an error.'),
-			null
-		]);
-	});
+  it('should return the error if the input contained the error', () => {
+    expect(
+      flatMap(x => [null, 'This worked!'])([
+        new Error('Sorry! Got an error.'),
+        null
+      ])
+    ).toMatchObject([new Error('Sorry! Got an error.'), null]);
+  });
 
-	it('should return the error if the map fn returned an error tuple', () => {
-		expect(flatMap((x) => [ new Error('Sorry! Got an error.'), null ])([ null, 'Hello' ])).toMatchObject([
-			new Error('Sorry! Got an error.'),
-			null
-		]);
-	});
+  it('should return the error if the map fn returned an error tuple', () => {
+    expect(
+      flatMap(x => [new Error('Sorry! Got an error.'), null])([null, 'Hello'])
+    ).toMatchObject([new Error('Sorry! Got an error.'), null]);
+  });
 });
 
 describe('withDefault', () => {
-	it('should return the original value if it did not contain the error', () => {
-		expect(withDefault(2)([ null, 1 ])).toBe(1);
-	});
+  it('should return the original value if it did not contain the error', () => {
+    expect(withDefault(2)([null, 1])).toBe(1);
+  });
 
-	it('should return the default value if the input contained error', () => {
-		expect(withDefault(1)([ new Error('No number!'), null ])).toBe(1);
-	});
+  it('should return the default value if the input contained error', () => {
+    expect(withDefault(1)([new Error('No number!'), null])).toBe(1);
+  });
 });
 
 describe('mapError', () => {
-	it('should not modify the input if it did not contain the error', () => {
-		expect(mapError(() => new Error('New error!'))([ null, 1 ])).toStrictEqual([ null, 1 ]);
-	});
+  it('should not modify the input if it did not contain the error', () => {
+    expect(mapError(() => new Error('New error!'))([null, 1])).toStrictEqual([
+      null,
+      1
+    ]);
+  });
 
-	it('should update the error if it was passed as an argument', () => {
-		expect(mapError(() => new Error('New error!'))([ new Error('Boom!'), null ])).toStrictEqual([
-			new Error('New error!'),
-			null
-		]);
-	});
+  it('should update the error if it was passed as an argument', () => {
+    expect(
+      mapError(() => new Error('New error!'))([new Error('Boom!'), null])
+    ).toStrictEqual([new Error('New error!'), null]);
+  });
+
+  it('throw if no function provided', () => {
+    expect(() => mapError()).toThrow(
+      'mapError: needs a function to transform the error.'
+    );
+  });
+});
+
+describe('pipe', () => {
+  it('should pipe the value through', () => {
+    const p = pipe(
+      map(x => parseInt(x)),
+      map(x => x + 1),
+      withDefault(5)
+    );
+
+    expect(p([null, '10'])).toBe(11);
+  });
+
+  it('throw if no function provided', () => {
+    expect(() => pipe()).toThrow(
+      'pipe: please pass at least one function to pipe the value through.'
+    );
+  });
+
+  it('should call functions from left to right', () => {
+
+  });
 });
--- src/fun.js
+++ src/fun.js
@@ -1,72 +1,98 @@
-// This module is purely experimental and not ready to be used.
+// XXX: This module is purely experimental and not ready to be used.
 const { chill, fail } = require('./chill');
 
-function isError([ err ]) {
-	return !!err;
+function isFunction(fn) {
+  return typeof fn === 'function';
 }
 
-function fst([ x ]) {
-	return x;
+function isError([err]) {
+  return !!err;
 }
 
-function snd([ _, y ]) {
-	return y;
+function fst([x]) {
+  return x;
 }
+
+function snd([_, y]) {
+  return y;
+}
+
+function log(label, x) {
+  if (!x) {
+    console.log(label);
+    return label;
+  }
+  console.log(label, x);
+  return x;
+}
+
+log.off = function off(label, x) {
+  if (!x) {
+    return label;
+  }
+  return x;
+};
 
 function map(fn) {
-	return (m) => (isError(m) ? m : chill(fn)(snd(m)));
+  if (!isFunction(fn)) {
+    throw new Error('map: needs a function to transform the value.');
+  }
+  return function(m) {
+    return isError(m) ? m : chill(fn)(snd(m));
+  };
 }
 
-// TODO:
-// function compose() {
-//
-// }
+function pipe(...fns) {
+  if (fns.length === 0) {
+    throw new Error(
+      'pipe: please pass at least one function to pipe the value through.'
+    );
+  }
+  return function(arg) {
+    return fns.reduce((acc, curr) => curr.call(null, log.off('acc', acc)), arg);
+  };
+}
 
 function mapError(fn) {
-	return function(m) {
-		if (isError(m)) {
-			const res = chill(fn)(fst(m));
+  if (!isFunction(fn)) {
+    throw new Error('mapError: needs a function to transform the error.');
+  }
+  return function(m) {
+    if (isError(m)) {
+      const res = chill(fn)(fst(m));
 
-			if (isError(res)) {
-				return res;
+      if (isError(res)) {
+        return res;
       }
 
-			return fail(snd(res));
-		}
+      return fail(snd(res));
+    }
 
-		return m;
-	};
+    return m;
+  };
 }
 
-// TODO:
-// const chilledFn = compose(
-//   chill,
-//   map(x => x + 1),
-//   flatMap(x => x > 5 ? success(x) : fail(new Error(`${x} is out of range!`))),
-//   withDeafault(5),
-// )(fn)
-
-// chilledFn(15) // 5
-
 function flatMap(fn) {
-	return function(m) {
-		if (isError(m)) {
-			return m;
-		}
-		const r = chill(fn)(snd(m));
-		return isError(r) ? r : snd(r);
-	};
+  return function(m) {
+    if (isError(m)) {
+      return m;
+    }
+    const r = chill(fn)(snd(m));
+    return isError(r) ? r : snd(r);
+  };
 }
 
 function withDefault(v) {
-	return function(m) {
-		return isError(m) ? v : snd(m);
-	};
+  return function(m) {
+    return isError(m) ? v : snd(m);
+  };
 }
 
 module.exports = {
-	map,
-	mapError,
-	flatMap,
-	withDefault
+  map,
+  mapError,
+  flatMap,
+  withDefault,
+  pipe,
+  log
 };
